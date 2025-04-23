c5cb1af2d5b15e8667c1e51fff7e9f73af4a82d7
ChannWaturandang <chanwaturandang22@gmail.com>
2025-04-23 17:38:42 +0800

    #4 commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,9 @@
 
 <head>
     @vite(['resources/css/app.css'])
-
+    @livewireStyles
+    @livewireScripts
+    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
